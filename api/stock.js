68884fa7380c2b10b483913d4b09452a80dbf735
yoshi0415-web <yoridaiiti@gmail.com>
2026-04-18 02:31:12 +0900

    Update date range in stock API request

--- api/stock.js
+++ api/stock.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const apiKey = process.env.JQUANTS_API_KEY;
 
     const response = await fetch(
-      "https://api.jquants.com/v2/equities/bars/daily?code=72030&from=20240122&to=20240210",
+      "https://api.jquants.com/v2/equities/bars/daily?code=72030&from=20240124&to=20240210",
       {
         headers: {
           "x-api-key": apiKey
