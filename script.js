a0020920cef8e27a45685c8b36ddb7eb81582310
yoshi0415-web <yoridaiiti@gmail.com>
2026-04-18 05:13:49 +0900

    Add loadChart function to render stock chart
    Implement loadChart function to fetch and display stock data.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let chart;
 
+function loadChart() {
+
+  const code = document.getElementById("code").value;
+
+  fetch(`https://stock-api-weld-three.vercel.app/api/stock?code=${code}`)
+    .then(res => res.json())
+    .then(data => {
+
+      const prices = data.data;
+
+      const labels = prices.map(d => d.Date);
+      const close = prices.map(d => d.C);
+
+      if (chart) chart.destroy();
+
+      chart = new Chart(document.getElementById('chart'), {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            data: close
+          }]
+        }
+      });
+
+    });
+}
