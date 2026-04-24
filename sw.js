64baf545035d70c2290cbacbca547e1d460bbe50
yoshi0415-web <yoridaiiti@gmail.com>
2026-04-25 05:27:30 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,44 +1,19 @@
-const CACHE_NAME = "kabutree-cache-2026.04.19-8";
-
-const STATIC_FILES = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon1.0.PNG"
-];
-
 self.addEventListener("install", event => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+    clients.claim().then(() =>
+      caches.keys().then(keys =>
+        Promise.all(keys.map(key => caches.delete(key)))
       )
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-
-  // APIはキャッシュしない
-  if (url.pathname.startsWith("/api/")) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
