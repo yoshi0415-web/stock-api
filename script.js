e4ee406fd01b7b188b73331c654a154681e12a6c
yoshi0415-web <yoridaiiti@gmail.com>
2026-04-25 09:12:07 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -121,6 +121,27 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(error);
     }
   }
+  
+  function isRedThreeSoldiers(prices) {
+  const last3 = prices.slice(-3);
+
+  if (last3.length < 3) {
+    return false;
+  }
+
+  const [day1, day2, day3] = last3;
+
+  const allBullish =
+    day1.C > day1.O &&
+    day2.C > day2.O &&
+    day3.C > day3.O;
+
+  const closeRising =
+    day1.C < day2.C &&
+    day2.C < day3.C;
+
+  return allBullish && closeRising;
+}
 
   if (resultList.children.length === 0) {
     resultList.innerHTML = "<li>該当なし</li>";
